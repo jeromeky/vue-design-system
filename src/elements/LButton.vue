3233e17dde617ffc137cabb588d247b8ae411d43
jerome ky <jeromeky@gmail.com>
2018-03-31 02:02:27 +1100

    add button

---
+++ src/elements/LButton.vue
@@ -0,0 +1,52 @@
+<template>
+  <button :class="['button ', variation]" v-on:click="onClick">
+    <slot/>
+  </button>
+</template>
+
+<script>
+  export default {
+    name: "LButton",
+    status: "prototype",
+    release: "0.0.1",
+    props: {
+      onClick: {
+        type: String,
+        required: false
+      },
+      variation: {
+        type: String,
+        default: "default",
+        validator: value => {
+          return value.match(/(default|disabled|strong|positive|negative)/)
+        },
+      },
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+  .button {
+    font-family: $font_family_text;
+    background: $color_white;
+    color: $color_oxford_blue;
+    &.positive {
+      background: $color_vermilion_darker;
+      color: $color_oxford_blue;
+    }
+    &.negative {
+      background: $color_vermilion_darker;
+      color: $color_vermilion_light;
+    }
+  }
+</style>
+
+<docs>
+  ```jsx
+  <div>
+    <l-button>Default</l-button>
+    <l-button variation="positive">Save</l-button>
+    <l-button variation="negative">Cancel</l-button>
+  </div>
+  ```
+</docs>
---
+++ src/patterns/ActionButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <l-button variation="positive" :on-click="action">Save</l-button>
+    <l-button variation="negative" :on-click="action">Cancel</l-button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ActionButton",
+    status: "prototype",
+    release: "0.0.1",
+    methods: {
+      action: function() {
+        alert('test');
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+</style>
+
+
+<docs>
+  ```jsx
+  <div>
+    <action-button/>
+  </div>
+  ```
+</docs>
